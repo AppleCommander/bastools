46befa17925036a434e563cc93d5fcb1506c9ce7
Rob Greene <robgreene@gmail.com>
2025-10-19 13:23:26 -0700

    Updating project to run CLTH tests during nativeCompile as well.

---
+++ scripts/run-clth-tests.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+DIR=".bin"
+if [ ! -d ${DIR} ]
+then
+  mkdir ${DIR}
+fi
+
+CLTH="${DIR}/clth.jar"
+if [ ! -f ${CLTH} ]
+then
+  eval $(cat build.gradle | grep clthVersion | tr -d ' ')
+  if [ -z "${clthVersion}" ]
+  then
+    echo "clthVersion was not found"
+    return 1
+  fi
+
+  URL="https://github.com/a2geek/command-line-test-harness/releases/download/${clthVersion}/clth-${clthVersion}.jar"
+  curl -o ${CLTH} -L ${URL}
+fi
+
+java -jar ${CLTH} tools/tests/src/test/resources/{bt,st}-config.yml
